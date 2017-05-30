bcf959617497e8cb63f519bf127993bf930a7a31
petromath77 <petromath77@gmail.com>
2017-05-31 00:37:28 +0300

    first_checked_ang

---
+++ app/js/controllers/realt.controller.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Admin on 31.05.2017.
+ */
+
+var realtApp = angular.module('realtApp', []);
+
+realtApp.controller('myCtrl', ['$scope',function($scope) {
+    $scope.name = "wewee";
+}]);
