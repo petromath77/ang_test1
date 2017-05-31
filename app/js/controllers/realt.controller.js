09f8501df91cd3b73d2c77ea7092d394008d4554
petromath77 <petromath77@gmail.com>
2017-06-01 02:45:56 +0300

    route_probl

--- app/js/controllers/realt.controller.js
+++ app/js/controllers/realt.controller.js
@@ -1,9 +1,38 @@
 /**
  * Created by Admin on 31.05.2017.
  */
+'use strict';
 
-var realtApp = angular.module('realtApp', []);
+var realtApp = angular.module('realtApp', ['ngRoute']);
 
+
+
+realtApp.config(['$routeProvider', function($routeProvider){
+    $routeProvider
+        .when('/home',{
+            templateUrl:'template/home.html',
+            controller:''
+        })
+        .when('/blog',{
+            templateUrl:'template/blog.html',
+            controller:''
+        })
+        .when('/apartments',{
+            templateUrl:'template/apartments.html',
+            controller:''
+        })
+        .when('/testimonials',{
+            templateUrl:'template/testimonials.html',
+            controller:''
+        })
+        .when('/contact',{
+            templateUrl:'template/contact.html',
+            controller:''
+        })
+        .otherwise({
+            redirectTo: '/home'
+        });
+}]);
 realtApp.controller('myCtrl', ['$scope',function($scope) {
     $scope.name = "wewee";
 }]);
