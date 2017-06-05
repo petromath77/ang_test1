c44f1f050b985b0d528333d5190e4655a1f99061
petromath77 <petromath77@gmail.com>
2017-06-05 17:39:36 +0400

    nav_work

--- app/js/common.js
+++ app/js/common.js
@@ -3,35 +3,46 @@ $(document).ready(function() {
 	// Custom JS
  	
 
-	/*var api = $('#menu').data('panel');
-	api.bind('open:finish', function(){
-		$('.hamburger').addClass('is-active');
-		$('a.logo').css('opacity', '0');
-	}).bind('close:finish', function(){
-		$('.hamburger').removeClass('is-active');
+	//var api = $('#menu').data('panel');
+	$('a.hamburger').on('click', function(){
+		var has_cls = $('a.hamburger').hasClass('is-active');
+			!has_cls ? $(this).addClass('is-active') : $(this).removeClass('is-active');
+	});
+
+	$('#menu li').on('click', function(){
+        $('#menu li').removeClass('active_nav');
+        $(this).addClass('active_nav');
+    });
+    $('.cont_butt').on('click', function(){
+        $('#menu li').removeClass('active_nav');
+        $('.cont').addClass('active_nav');
+    });
+
+    
+
+	/*
+	$('.hamburger').on('click', function(){
+		$(this).removeClass('is-active');
 		$('a.logo').css('opacity', '1');
-	});*/
-     $('#trig_mix').on('click', function(){
+	});
+
+     $('a#trig_mix').on('click', function(){
+
              $(".mix_wrap").mixItUp({
-                 controls: {
-                     enable: true
-                 },
-                 layout: {
-                     display: "inline-block"
-                 },
                  callbacks: {
                      onMixLoad: function(){
-                         alert("Load!");
                      }
                  }
              });
 
      });
+     
     $('.filter_wrap li').on('click', function(){
         $('.filter_wrap li').removeClass('active');
         $(this).addClass('active');
     });
 
+*/
 	$('.menu-link').bigSlide();
 
 
